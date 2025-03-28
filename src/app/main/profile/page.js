da5462122287e32956dbdbeba60e0369e39e19f9
phoebejeske <phoebe.jeske@gmail.com>
2025-03-28 08:54:59 -0500

    minor changes

--- src/app/main/profile/page.js
+++ src/app/main/profile/page.js
@@ -4,7 +4,6 @@ export default function Profile() {
   return (
     <div className = "p-6">
       <h1 className="text-2xl font-bold">Your Profile</h1>
-      <p>View and manage your account details.</p>
       
       <div className="mt-6 grid gap-4 md:grid-cols-2">
         <Link href="/main/profile/boards">
